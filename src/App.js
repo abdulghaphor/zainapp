579a31bc68fb83024f4bb766eb6509e9280107b2
Abdulghaphor Hajjieh <ghaphorhajjieh@gmail.com>
2021-03-04 20:07:47 +0300

    state example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import { Container, Button, Col, Row } from "react-bootstrap";
+import falafel from "./movies";
 function App() {
+  const [watchList, setWatchList] = useState(
+    falafel.map((cv) => ({ ...cv, status: false }))
+  );
+  const switchFn = (id) => {
+    setWatchList(
+      watchList.map((shawerma) =>
+        shawerma.id === id
+          ? { ...shawerma, status: !shawerma.status }
+          : shawerma
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <h1>Watch List</h1>
+          {watchList
+            .filter((cv) => cv.status === false)
+            .map((cv) => (
+              <Row style={{ marginBottom: "5px" }}>
+                <Col>{cv.name}</Col>
+                <Col>
+                  <Button onClick={() => switchFn(cv.id)}>Watched</Button>
+                </Col>
+              </Row>
+            ))}
+        </Col>
+
+        <Col>
+          <h1>Watched</h1>
+          {watchList
+            .filter((cv) => cv.status === true)
+            .map((cv) => (
+              <Row style={{ marginBottom: "5px" }}>
+                <Col>{cv.name}</Col>
+                <Col>
+                  <Button onClick={() => switchFn(cv.id)}>Unwatch</Button>
+                </Col>
+              </Row>
+            ))}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/movies.js
@@ -0,0 +1,11 @@
+const movies = [
+  { name: "American Beauty", id: 1 },
+  { name: `The Devil's Advocate`, id: 2 },
+  { name: "The Matrix", id: 3 },
+  { name: "There Will Be Blood", id: 4 },
+  { name: "Syriana", id: 5 },
+  { name: "The Fast and the Furious", id: 6 },
+  { name: "Whiplash", id: 7 },
+  { name: "Parasite", id: 8 },
+];
+export default movies;
